43613fce6280b35e3878f72e939a1b173fe0cf77
nicolelfporca <113660629+nicolelfporca@users.noreply.github.com>
2023-11-26 13:24:08 +0800

    SideNav: changed icon layout

--- database/seeders/SideNav.php
+++ database/seeders/SideNav.php
@@ -16,85 +16,85 @@ class SideNav extends Seeder
         //1
         $SideNav = new SideNavModel();
         $SideNav->name = 'ADMIN';
-        $SideNav->mdi_icon = 'mdi-view-dashboard';
+        $SideNav->mdi_icon = 'mdi-view-grid';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //2
         $SideNav = new SideNavModel();
         $SideNav->name = 'APPLICATION';
-        $SideNav->mdi_icon = 'mdi-playlist-check';
+        $SideNav->mdi_icon = 'mdi-application-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //3
         $SideNav = new SideNavModel();
         $SideNav->name = 'HOME';
-        $SideNav->mdi_icon = 'mdi-home';
+        $SideNav->mdi_icon = 'mdi-home-variant-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //4
         $SideNav = new SideNavModel();
         $SideNav->name = 'REQUIREMENT DETAILS';
-        $SideNav->mdi_icon = 'mdi-format-list-checkbox';
+        $SideNav->mdi_icon = 'mdi-attachment';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //5
         $SideNav = new SideNavModel();
         $SideNav->name = 'STORE';
-        $SideNav->mdi_icon = 'mdi-store';
+        $SideNav->mdi_icon = 'mdi-store-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //6
         $SideNav = new SideNavModel();
         $SideNav->name = 'USER ROLE';
-        $SideNav->mdi_icon = 'mdi-account-multiple';
+        $SideNav->mdi_icon = 'mdi-account-multiple-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //7
         $SideNav = new SideNavModel();
         $SideNav->name = 'DELIVERY';
-        $SideNav->mdi_icon = 'mdi-truck-delivery';
+        $SideNav->mdi_icon = 'mdi-walk';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //8
         $SideNav = new SideNavModel();
         $SideNav->name = 'APPLICANTS';
-        $SideNav->mdi_icon = 'mdi-badge-account-horizontal';
+        $SideNav->mdi_icon = 'mdi-account-card-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //9
         $SideNav = new SideNavModel();
         $SideNav->name = 'INVENTORY';
-        $SideNav->mdi_icon = 'mdi-cube';
+        $SideNav->mdi_icon = 'mdi-package-variant-closed';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //10
         $SideNav = new SideNavModel();
         $SideNav->name = 'PROFILE';
-        $SideNav->mdi_icon = 'mdi-account';
+        $SideNav->mdi_icon = 'mdi-account-circle-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //11
         $SideNav = new SideNavModel();
         $SideNav->name = 'PRODUCT TYPE DETAILS';
-        $SideNav->mdi_icon = 'mdi-package-variant-closed';
+        $SideNav->mdi_icon = 'mdi-package-variant';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //12
         $SideNav = new SideNavModel();
         $SideNav->name = 'STORE TYPE DETAILS';
-        $SideNav->mdi_icon = 'mdi-format-list-text';
+        $SideNav->mdi_icon = 'mdi-storefront-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //13
         $SideNav = new SideNavModel();
         $SideNav->name = 'TOPUP';
-        $SideNav->mdi_icon = 'mdi-cash';
+        $SideNav->mdi_icon = 'mdi-credit-card-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
         //14
         $SideNav = new SideNavModel();
         $SideNav->name = 'ORDERS';
-        $SideNav->mdi_icon = 'mdi-power';
+        $SideNav->mdi_icon = 'mdi-cart-outline';
         $SideNav->pic_icon = '';
         $SideNav->save();
     }
