f872dd642e924c3f691035ddb363c1eac07dcc29
Daneris Mendoza <danerismendoza096@gmail.com>
2024-02-06 15:22:12 +0800

    Transaction model: seller details add

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -11,4 +11,24 @@ class Transaction extends Model
     protected $fillable = [
         'delivery_id',
     ];
+
+    public function orderDetails()
+    {
+        return $this->hasOne(Order::class, 'transaction_id', 'id');
+    }
+
+    public function storeDetails()
+    {
+        return $this->orderDetails->hasOne(Store::class, 'id', 'store_id');
+    }
+
+    public function userRoleDetails()
+    {
+        return $this->storeDetails->hasOne(UserRole::class, 'id', 'user_role_id');
+    }
+
+    public function sellerDetails()
+    {
+        return $this->userRoleDetails->hasOne(UserDetail::class, 'id', 'user_id');
+    }
 }
